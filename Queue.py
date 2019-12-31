924f12ec87c8e1ecfa6746d886c6fbc00a455af0
ADITHYAKINECT <adithyamaths.1995@gmail.com>
2019-12-31 16:34:28 +0100

    Reverse Method Added

--- Queue.py
+++ Queue.py
@@ -1,42 +1,4 @@
-
-class stack:
-    
-    def __init__(self,size):
-        self.size = size
-        self.list = [None for i in range(size)]
-        self.top = -1
-
-    def is_empty(self):
-        return self.top != -1 
-    
-    def is_full(self):
-        return self.top == self.size - 1
-
-    def push(self,element):
-        if self.is_full():
-            print("Stack is Overflowing!, cannot push {} into the stack".format(element))
-        else:
-            self.top += 1
-            self.list[self.top] = element 
-
-    def pop(self):
-        if not self.is_empty():
-            print("Stack is Empty, cannot perfrom pop operation")
-        else:
-            posterior = self.list[self.top]
-            self.top -= 1  
-            return posterior
-    
-    def display(self):
-        if not self.is_empty():
-            print("Stack is Empty")
-        else:
-            print("Stack is: ")
-            position = self.top
-            while position > -1:
-                print(self.list[position],end=" ")
-                position -= 1    
-            print("\n")
+from stack import stack
 
 class Queue:
 
@@ -111,3 +73,10 @@ class Queue:
                 print(self.list[position],end=" ")
                 position += 1
             print("\n")
+
+
+Q = Queue(10)
+Q.enqueue(100)
+Q.enqueue(200)
+Q.reverse()
+Q.display()
